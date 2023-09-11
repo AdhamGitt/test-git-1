9f156dd1cd194d579ef8582ced02ce7d83ca1d87
MaXiM <adham.gamal21@gmail.com>
2023-09-11 16:42:14 +0200

    initial project, added empty home screen.

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:git_practice/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
